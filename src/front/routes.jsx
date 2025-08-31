8bfd677b811d740ebcd3f0a1cfd2e301067e9f41
gabrieeel2109 <gabrielof219@icloud.com>
2025-08-31 13:43:17 +0000

    avance calculadora

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -6,9 +6,10 @@ import {
   Route,
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/public/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+import {Calculadora} from "/workspaces/fs-pt-115-CuatroK/src/front/components/public/Calculadora.jsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +25,7 @@ export const router = createBrowserRouter(
       {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
       <Route path="/" element={<Home />} />
       <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
+      <Route path="/calculadora" element={<Calculadora />} />
     </Route>
   )
 );
