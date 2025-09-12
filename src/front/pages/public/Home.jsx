d727af4ace74b4e89e6044f4799c1fb5b1409d86
BlackoutMinus <157079894+BlackoutMinus@users.noreply.github.com>
2025-09-12 18:11:34 +0000

    arreglando arreglos

--- src/front/pages/public/Home.jsx
+++ src/front/pages/public/Home.jsx
@@ -6,32 +6,6 @@ import { Carrusel_principal } from "../../components/public/Carrusel_principal.j
 export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
 
-  const loadMessage = async () => {
-    try {
-      const backendUrl = import.meta.env.VITE_BACKEND_URL;
-      if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file");
-
-      const response = await fetch(`${backendUrl}/api/hello`);
-      const data = await response.json();
-
-      if (response.ok) {
-        dispatch({ type: "set_hello", payload: data.message });
-      } else {
-        console.error("Backend responded with error:", data);
-      }
-      return data;
-    } catch (error) {
-      console.error(
-        "Could not fetch the message from the backend. Please check if the backend is running and the backend port is public.",
-        error
-      );
-    }
-  };
-
-  useEffect(() => {
-    loadMessage();
-  }, []);
-
   return (
     <>
       <Carrusel_principal />
