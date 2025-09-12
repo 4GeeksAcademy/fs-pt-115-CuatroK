728402205c54667ffb2ef470952555ca7f4d9937
gabrieeel2109 <gabrielof219@icloud.com>
2025-09-12 18:22:12 +0000

    cambios de logo

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -9,14 +9,14 @@ import "./navbarStyles.css";
 
 export const Navbar = () => {
   const [menuAbierto, setMenuAbierto] = useState(false);
-  
+
 
   return (
     <div>
       <nav className="navbar" style={{ backgroundColor: "#5C3D2E" }}>
-        <div className="d-flex justify-content-between align-items-center w-100 px-3"> 
+        <div className="d-flex justify-content-between align-items-center w-100 px-3">
 
-          
+
           <button
             className="navbar-toggler d-lg-none"
             type="button"
@@ -26,18 +26,18 @@ export const Navbar = () => {
             <span >☰</span>
           </button>
 
-          
+
           <div className="navbar-logo d-none d-lg-block">
             <Link to="/">
               <img
-                src="https://media.discordapp.net/attachments/1409165086697848872/1410329949633642556/LogoEntero.PNG?ex=68b68e5e&is=68b53cde&hm=ff73ccf8bfef9e0e99242e20a380d812cd8a38ba672c3daa85877141bed33574&=&format=webp&quality=lossless&width=360&height=334"
+                src="/logo4k.png"
                 alt="Logo"
                 style={{ width: 50, height: 44 }}
               />
             </Link>
           </div>
 
-          
+
           <div className="d-flex align-items-center gap-2 fixed-nav-tools">
             <SearchBar />
             <ShoppingCart />
@@ -45,7 +45,7 @@ export const Navbar = () => {
           </div>
         </div>
 
-        
+
         {menuAbierto && (
           <div className="d-lg-none px-3 py-2">
             <NavAndTabs />
--- src/front/pages/public/Home.jsx
+++ src/front/pages/public/Home.jsx
@@ -1,36 +1,9 @@
-import React, { useEffect } from "react";
-import useGlobalReducer from "../../hooks/useGlobalReducer.jsx";
+
 import { CarruselSecundario } from "../../components/public/CarruselSecundario.jsx";
 import { Carrusel_principal } from "../../components/public/Carrusel_principal.jsx";
 
 export const Home = () => {
-  const { store, dispatch } = useGlobalReducer();
 
-  const loadMessage = async () => {
-    try {
-      const backendUrl = import.meta.env.VITE_BACKEND_URL;
-      if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file");
-
-      const response = await fetch(`${backendUrl}/api/hello`);
-      const data = await response.json();
-
-      if (response.ok) {
-        dispatch({ type: "set_hello", payload: data.message });
-      } else {
-        console.error("Backend responded with error:", data);
-      }
-      return data;
-    } catch (error) {
-      console.error(
-        "Could not fetch the message from the backend. Please check if the backend is running and the backend port is public.",
-        error
-      );
-    }
-  };
-
-  useEffect(() => {
-    loadMessage();
-  }, []);
 
   return (
     <>
